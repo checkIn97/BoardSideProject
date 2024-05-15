d76494c6038803a7701125a950414b90c9a0088e
JeongSan <vnfmstks1733@naver.com>
2024-05-15 15:21:12 +0900

    #1 - data rest 설정 게시글, 댓글의 Json api를 자동으로 restful하게 만들게끔 설정

--- src/main/java/com/fastcampus/boardsideproject/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/boardsideproject/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.fastcampus.boardsideproject.repository;
 
 import com.fastcampus.boardsideproject.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
